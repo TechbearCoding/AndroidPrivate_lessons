a3cb32c3a8c03bff85998768d2df57aaca02c454
Martins Leitarts <martins@techspace.lv>
2021-04-13 20:13:08 +0300

    numbers and clear work

--- Calc/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Calc/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.calculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val editResult = findViewById<EditText>(R.id.editResult)
+        editResult.setText("0")
 
         val btnZero = findViewById<Button>(R.id.btnZero)
         setNumberButtonListener(btnZero)
@@ -39,12 +43,20 @@ class MainActivity : AppCompatActivity() {
 
         val btnNine = findViewById<Button>(R.id.btnNine)
         setNumberButtonListener(btnNine)
-    }
 
+        val btnClear = findViewById<Button>(R.id.btnClear)
+        btnClear.setOnClickListener(){
+            editResult.setText("0")
+        }
+    }
 
     private fun setNumberButtonListener(btn: Button){
         btn.setOnClickListener(){
-
+            val editResult = findViewById<EditText>(R.id.editResult)
+            if(editResult.text.toString() == "0" || editResult.text.toString().contains(' '))
+                editResult.setText(btn.text.toString())
+            else
+                editResult.setText(editResult.text.toString() + btn.text.toString())
         }
     }
 }
